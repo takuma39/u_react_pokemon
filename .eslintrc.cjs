57a906a36587638f11cf11caf9ae2c5aa7104fdd
takuma39 <takuma.sato.job@gmail.com>
2024-03-20 22:00:51 +0900

    SETUP-0000：add：ESLint 導入

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,64 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs','vite.config.js'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        format: ["camelCase"],
+      },
+      {
+        selector: "function",
+        format: ["camelCase", "PascalCase"],
+      },
+      {
+        selector: "parameter",
+        format: ["camelCase"],
+      },
+      {
+        selector: "class",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "method",
+        format: ["camelCase"],
+      },
+      {
+        selector: "property",
+        format: ["camelCase"],
+      },
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "typeAlias",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "typeParameter",
+        format: ["camelCase"],
+      },
+      {
+        selector: "enum",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "enumMember",
+        format: ["UPPER_CASE"],
+      },
     ],
   },
 }
